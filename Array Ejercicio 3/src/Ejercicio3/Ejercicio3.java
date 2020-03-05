1256cfb27d589fb28241220e8c8f0feb9f7aeb12
Pablo223 <37366362+Pablo223@users.noreply.github.com>
2020-03-05 13:56:07 +0100

    Ejercicio 3

---
+++ Array Ejercicio 3/src/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,27 @@
+package Ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		double[] a;
+		a = new double[20];
+		double suma = 0;
+		int cont = 0;
+		
+		
+	for (int b=0;b<a.length;b++) {
+		//System.out.println("Dame un numero par");
+		cont = cont+2;
+		a[b]=cont;
+	}	
+	for (int y=0;y<a.length;y++) {			
+	System.out.println("Los 20 numeros pares "+a[y]);
+		}	
+	}
+	}
+
