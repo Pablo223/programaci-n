3bd1b2fe52c69ca63968a0368650dcbe70904a66
Pablo223 <37366362+Pablo223@users.noreply.github.com>
2020-03-11 13:24:17 +0100

    Ejercicio 6

---
+++ Array ejercicio 6/src/ejercicio6/ejercicio6.java
@@ -0,0 +1,20 @@
+package ejercicio6;
+
+public class ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] num;
+		num = new int[10];
+		int aleatorio = (int) Math.random();		
+		
+		for (int i=0;i<num.length;i++) {
+		  num[i]=(int) Math.random();
+			System.out.println(Math.random());
+		}
+	}
+
+	}
+
+
